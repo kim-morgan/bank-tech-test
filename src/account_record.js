eb18d111b69766ec7d756b1a41a1fbcaa8cf2b1c
Kimberley Morgan <kimberley.morgan135@gmail.com>
2022-01-31 16:19:13 +0000

    can render current date in correct format

--- src/account_record.js
+++ src/account_record.js
@@ -1,7 +1,19 @@
 class AccountRecord {
+
   printStatement() {
     return "date || credit || debit || balance";
   }
+
+  getCurrentDate(dateClassInstance = new Date()) {
+    let day = dateClassInstance.getDate();
+    let month = dateClassInstance.getMonth() + 1;
+    if (month < 10) {
+      month = `0${month}`;
+    }
+    let year = dateClassInstance.getFullYear();
+    return `${day}/${month}/${year}`;
+  }
+
 }
 
 module.exports = AccountRecord;
--- test/account_record.test.js
+++ test/account_record.test.js
@@ -1,8 +1,25 @@
 const AccountRecord = require("../src/account_record");
 
+let dateClassMock = {
+  getFullYear() {
+    return 2022;
+  },
+  getMonth() {
+    return 0;
+  },
+  getDate() {
+    return 31;
+  }
+}
+
 describe("AccountRecord", () => {
   it("Can print a statement, initializing with just the blank header", () => {
     let record = new AccountRecord();
     expect(record.printStatement()).toBe("date || credit || debit || balance");
+  })
+  it("Can render the date in the intended format", () => {
+    let record = new AccountRecord();
+    console.log(record.getCurrentDate(dateClassMock));
+    expect(record.getCurrentDate(dateClassMock)).toBe("31/01/2022");
   })
 })
